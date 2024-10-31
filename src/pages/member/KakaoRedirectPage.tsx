ebfd023c8407e96e3a9ff5403bce6f6caee455d6
ssinyong <bsy2889@gmail.com>
2024-10-31 09:40:32 +0900

    Initial commit

---
+++ src/pages/member/KakaoRedirectPage.tsx
@@ -0,0 +1,37 @@
+import {useSearchParams} from "react-router-dom";
+import {useEffect} from "react";
+import {getAccessToken, getMemberWithAccessToken} from "../../api/kakaoAPI.ts";
+
+
+function KakaoRedirectPage() {
+
+    const [searchParams] = useSearchParams()
+
+    const authCode:string|null = searchParams.get("code")
+
+    useEffect(() => {
+
+        if(authCode != null){
+
+        getAccessToken(authCode).then(accessToken => {
+
+            console.log(accessToken)
+            getMemberWithAccessToken(accessToken).then(result => {
+                console.log("======================")
+                console.log(result)
+            })
+        })
+      }//end if
+
+    },[authCode])
+
+
+    return (
+        <div>
+            <div>Kakao Login Redirect</div>
+            <div>{authCode}</div>
+        </div>
+    );
+}
+
+export default KakaoRedirectPage;
